3e7429e8539f63c866f4c630abb206310f2c9f47
brycepc <brycepc@hotmail.com>
2025-01-18 08:13:26 +1300

    Updating deployment scripts

--- bin/deploy-login.sh
+++ bin/deploy-login.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-noExecuteChangeset=""
+samDeployBucket="MyBucketName"
+stackName="tbs-devops-toolkit-login-${environment}"
+environmentName="prod"
+route53AppDomainName="myroute53.domain.name"
+region="my aws region"
 
-if [ "${isNoExecuteChangeset}" = "true" ]; then
-echo "Setting no-execute-changeset."
-noExecuteChangeset="--no-execute-changeset"
-else
-echo "no-execute-changeset is false"
-fi
-
-echo Running against environment: ${environment}, account: $awsaccount_target
-cd ${workspaceDir}/devops-toolkit-login
-
-echo Running against region: $region, environment: $environment, account: $awsaccount_target, noexecutechangset: $noExecuteChangeset
+cd ./devops-toolkit-login
 
 sam build --template-file ./cloudformation/template.yaml --base-dir .
 
 sam deploy --stack-name $stackName \
---s3-bucket $samDeployBucket --s3-prefix sam/$stackName $noExecuteChangeset \
+--s3-bucket $samDeployBucket --s3-prefix sam/$stackName \
 --parameter-overrides \
 Environment=${environmentName} \
 Route53AppDomainName=$route53AppDomainName \
 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --region $region --no-confirm-changeset --no-fail-on-empty-changeset \
---tags stackname=$stackName environment=$environmentName system=$system customer=$customer
+--tags stackname=$stackName environment=$environmentName \
+--profile thebetterstore
--- bin/deploy-web.sh
+++ bin/deploy-web.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-noExecuteChangeset=""
+samDeployBucket="MyBucketName"
+appName="tbs-devops-toolkit-web"
+stackName=$appName-"${environment}"
+environmentName="prod"
+route53AppHostedZoneId="myRoute53HostedZoneId"
+route53AppDomainName="myroute53.domain.name"
+apidomain="My.Api.Domain.Name"
+region="my aws region"
 
-if [ "${isNoExecuteChangeset}" = "true" ]; then
-echo "Setting no-execute-changeset."
-noExecuteChangeset="--no-execute-changeset"
-else
-echo "no-execute-changeset is false"
-fi
-
-echo Running against environment: ${environment}, account: $awsaccount_target
-cd ${workspaceDir}/devops-toolkit-web
+cd ./devops-toolkit-web
 
 sam build --template-file ./cloudformation/template.yaml
-sam deploy --stack-name $stackName $noExecuteChangeset \
+sam deploy --stack-name $stackName \
 --parameter-overrides \
 Environment=${environmentName} \
 Route53AppHostedZoneId=$route53AppHostedZoneId \
 Route53AppDomainName=$route53AppDomainName \
 ApiDomain=${apiDomain} \
 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --region $region --no-confirm-changeset --no-fail-on-empty-changeset \
---tags stackname=$stackName environment=$environmentName
+--tags stackname=$stackName environment=$environmentName \
+--profile thebetterstore
