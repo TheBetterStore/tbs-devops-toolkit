f0745a45a9e40ec24d55126893caa6d1d2961bb5
brycepc <brycepc@hotmail.com>
2025-07-21 08:50:09 +1200

    Update stack params UI

--- devops-toolkit-api/src/application/handlers/describe-parameters/index.ts
+++ devops-toolkit-api/src/application/handlers/describe-parameters/index.ts
@@ -24,7 +24,7 @@ exports.handler = async (event: APIGatewayEvent) => {
     return response;
   }
 
-  let pageSize = 10;
+  let pageSize = 100;
   let filters: ParameterStringFilter[] = [];
   let nextToken = '';
   let region = '';
@@ -40,7 +40,7 @@ exports.handler = async (event: APIGatewayEvent) => {
     }
 
     filters = JSON.parse(event.queryStringParameters['filters'] + '');
-    pageSize = Number(event.queryStringParameters['pageSize'] || 10);
+    pageSize = Number(event.queryStringParameters['pageSize'] || 100);
     nextToken = event.queryStringParameters['nextToken'] + '';
     region = params.region + '';
 
